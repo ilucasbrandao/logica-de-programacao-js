f193d480337bbb905593e1724b02296be1e01cc7
ilucasbrandao <dev.lucasbrandao@gmail.com>
2026-02-23 16:48:55 -0300

    metodos criados: remove, pop, reverse  e print

--- conceitos/estruturaDeDados/tryAlone/linkedList.js
+++ conceitos/estruturaDeDados/tryAlone/linkedList.js
@@ -26,4 +26,91 @@ class LinkedList {
     }
     this.#size++;
   }
+
+  remove() {
+    let current = this.head;
+    let previous = null;
+
+    while (current) {
+      if (current.value === current) {
+        if (previous) {
+          previous.next = current.next;
+        }
+        if (current === this.tail) {
+          this.tail = previous.next;
+        } else {
+          this.head = current.next;
+
+          if (current === this.tail) {
+            this.tail = null;
+          }
+        }
+      }
+    }
+    this.#size--;
+    previous = current;
+    current = current.next;
+  }
+
+  pop() {
+    if (this.tail) {
+      const last = this.tail;
+      const value = last.value;
+
+      if (this.#size === 1) {
+        this.head;
+      }
+    }
+
+    let current = this.head;
+    let prev = null;
+
+    while (current !== last) {
+      prev = current;
+      current = current.next;
+    }
+
+    prev.next = null;
+    this.tail = prev;
+    this.#size--;
+
+    return value;
+  }
+
+  reverse() {
+    let current = this.head;
+    let previous = null;
+    let next = null;
+
+    this.tail = this.head;
+
+    while (current) {
+      next = current.next;
+      current.next = previous;
+      previous = current;
+      current = next;
+    }
+    this.head = previous;
+  }
+
+  print() {
+    let current = this.head;
+    let result = "";
+
+    while (current) {
+      result += `${current.value}  ---> `;
+      current = current.next;
+    }
+
+    console.log(`${result} --- Size: ${this.#size}`);
+  }
 }
+
+const list = new LinkedList();
+
+list.append(10);
+list.append(20);
+list.append(30);
+list.print(); // 10 --> 20 --> 30
+list.reverse();
+list.print(); // 30 --> 20 --> 10
