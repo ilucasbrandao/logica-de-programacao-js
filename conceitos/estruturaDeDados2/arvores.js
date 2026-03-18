38b6fb0b7604ab677016b275ca467221ac189b2d
ilucasbrandao <dev.lucasbrandao@gmail.com>
2026-03-18 05:34:09 -0300

    create: studying binary tree, start in method insert.

---
+++ conceitos/estruturaDeDados2/arvores.js
@@ -0,0 +1,46 @@
+class Node {
+  constructor(key) {
+    this.key = key;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(key) {
+    const newNode = new Node();
+
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.#insertNode(this.root, newNode);
+    }
+  }
+
+  #insertNode(node, newNode) {
+    if (newNode.key < node.key) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.#insertNode(node.left, newNode);
+      }
+    } else if (newNode.key > node.key) {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.#insertNode(node.right, newNode);
+      }
+    }
+  }
+}
+
+const bst = new BinarySearchTree();
+bst.insert(20);
+bst.insert(15);
+bst.insert(18);
+
+console.log(bst);
